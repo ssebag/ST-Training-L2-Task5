29cb606b2b828c220a04ffe4a4b9b0d97f5f84f4
seba ghnem <s.sseba96@gmail.com>
2024-04-21 23:10:46 +0300

    update base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ST-L2-Task5",
+  base: "/ST-Training-L2-Task5",
   plugins: [react()],
 })
